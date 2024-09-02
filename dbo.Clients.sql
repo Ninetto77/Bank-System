843ea8045422c6cf9692e76eae54773008d99ca6
Ninetto77 <ndsingkh@stud.eltech.ru>
2024-09-02 17:07:48 +0300

    add: dbo.Clients

---
+++ dbo.Clients.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Clients] (
+    [Id]           INT        IDENTITY (1, 1) NOT NULL,
+    [departametId] NCHAR (10) NOT NULL,
+    [surname]      NCHAR (25) NOT NULL,
+    [name]         NCHAR (25) NOT NULL,
+    [middlename]   NCHAR (25) NULL,
+    [phone]        NCHAR (11) NULL,
+    [pasportData]  NCHAR (10) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
--- dbo.Table.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Users] (
-    [Id]         INT           NOT NULL IDENTITY,
-    [Login]      NVARCHAR (50) NOT NULL,
-    [Password]   NVARCHAR (50) NOT NULL,
-    [WorkerType] NVARCHAR (10) NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
