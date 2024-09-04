f942cdb9845f745680d2c45d0d479b1d5c582c00
Ninetto77 <ndsingkh@stud.eltech.ru>
2024-09-04 11:26:57 +0300

    upgrade: font

---
+++ BankWPF/AuthorizationWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using BankLogic_Library.MVP;
+using BankLogic_Library.MVP.Presenters;
+using System.Windows.Input;
+using System;
+
+namespace Lesson10
+{
+	/// <summary>
+	/// Логика взаимодействия для AuthorizationWindow.xaml
+	/// </summary>
+	public partial class AuthorizationWindow : Window, IViewAuthWindow
+	{
+		public string Login { get => LoginTextBox.Text; }
+		public string Password { get => PasswordTextBox.Password; }
+		public bool? isManager { get => WorkerCheckBox.IsChecked; }
+
+
+		private readonly AuthPresenter p;
+
+		/// <summary>
+		/// Конструктор
+		/// </summary>
+		public AuthorizationWindow()
+		{
+			p = new AuthPresenter(this);
+			p.OnEnterInSystem += OpenLoadWindow;
+			InitializeComponent();
+			InitButtons();
+		}
+
+		/// <summary>
+		/// Инициализировать кнопки
+		/// </summary>
+		private void InitButtons()
+		{
+			EnterBtn.Click += (s, e) => p.EnterToSystem();
+			RegisterBtn.Click += (s, e) => Task.Run(async () => await p.RegistrateInSystem());
+			CloseBtn.Click += (s, e) => Application.Current.Shutdown();
+		}
+
+
+		/// <summary>
+		/// Закрыть текущее окно, открыть окно загрузки
+		/// </summary>
+		/// <param name="worker"></param>
+		private void OpenLoadWindow(Worker worker)
+		{
+			this.Hide();
+			LoadWindow window = new LoadWindow(worker);
+			window.Show();
+		}
+
+		private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton == MouseButton.Left)
+			{
+				this.DragMove();
+			}
+		}
+	}
+}
---
+++ BankWPF/LoadWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using BankLogic_Library.MVP.Presenters;
+using BankLogic_Library.MVP.Views;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace Lesson10
+{
+	/// <summary>
+	/// Логика взаимодействия для LoadWindow.xaml
+	/// </summary>
+	public partial class LoadWindow : Window, IViewLoad
+	{
+		Worker worker;
+		LoadPresenter p;
+
+		public bool IsEnableButton
+		{
+			set
+			{
+				LoadClientsXMLBtn.IsEnabled = value;
+				LoadClientsDBBtn.IsEnabled = value;
+			}
+		}
+		public float ProgressBarValue { set => ClientProgressBar.Value = value; }
+		public string ProgressBarText { set => TextProgressBar.Text = value; }
+
+		public LoadWindow(Worker worker)
+		{
+			this.worker = worker;
+
+			p = new LoadPresenter(this, worker);
+			p.OnFinishLoading += ShowContinueButton;
+			InitializeComponent();
+			InitButtons();
+		}
+
+		/// <summary>
+		/// При окончании загрузки скрыть /открыть нужные кнопки
+		/// </summary>
+		private void ShowContinueButton()
+		{
+			ContinueBtn.Visibility = Visibility.Visible;
+			LoadClientsDBBtn.Visibility = Visibility.Hidden;
+			LoadClientsXMLBtn.Visibility = Visibility.Hidden;
+		}
+
+		#region Инициализация
+		///// <summary>
+		///// начальное окно, спрашивающее, является ли пользователь менеджером или консультантом
+		///// </summary>
+		///// <returns></returns>
+		//private Worker FindOutWorker()
+		//{
+		//	MessageBoxResult result = MessageBox.Show(
+		//	"Вы менеджер?",
+		//	 "Консультант или менеджер",
+		//	 MessageBoxButton.YesNo,
+		//	 MessageBoxImage.Information
+		//);
+
+		//	if (result == MessageBoxResult.Yes)
+		//		worker = new Manager();
+		//	else
+		//		worker = new Consultate();
+
+		//	return worker;
+		//}
+
+		/// <summary>
+		/// Инициализация кнопок
+		/// </summary>
+		private void InitButtons()
+		{
+			LoadClientsXMLBtn.Click += (s, e) => Task.Run(async () => await StartLoad("XML"));
+			LoadClientsDBBtn.Click += (s, e) => Task.Run(async () => await StartLoad("DB"));
+			CloseBtn.Click += (s, e) => Application.Current.Shutdown();
+		}
+		#endregion
+
+		private async Task StartLoad(string place)
+		{
+			this.Dispatcher.Invoke( () =>
+			{
+				p.ChangeButtons(true);
+			});
+
+			try
+			{
+				var task = p.StartLoad(place);
+				await task;
+
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+
+			this.Dispatcher.Invoke( () =>
+			{
+				p.ChangeButtons(false);
+			});
+		}
+
+		/// <summary>
+		/// Кнопка продолжения
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void ContinueBtn_Click(object sender, RoutedEventArgs e)
+		{
+			MainWindow main = new MainWindow(worker);
+			main.Show();
+
+			this.Close();
+		}
+
+
+	}
+}
